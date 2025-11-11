3f2142f9468a9a41a120dcd3d615973bf61e823f
Vaishnav Premjith <vaishnavpx@gmail.com>
2025-11-10 18:53:35 -0800

    11-10-2025

---
+++ reversestring.py
@@ -0,0 +1,7 @@
+string=input("Enter a string: ")
+string2=("")
+for i in string:
+    string2=i+string2
+
+print("Original string:", string)
+print("Reversed String:", string2)
---
+++ sumofwholenumbers.py
@@ -0,0 +1,6 @@
+
+num=int(input("Enter how many times you want to add: "))
+sum=0
+for i in range(1,num+1):
+    sum=sum+i
+    print(f"sum is equals to {sum}.")
