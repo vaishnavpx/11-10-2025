4381cc846686fbde2ecde001af97ed1aaaff2ce0
Vaishnav Premjith <vaishnavpx@gmail.com>
2026-01-12 19:09:46 -0800

    shut down(functions)

---
+++ AfterClassProjects/shutdown.py
@@ -0,0 +1,8 @@
+def shut_down():
+    ans=input("Do you want to shut down?(yes/no): ")
+    if ans == "yes":
+        print("SHUTING DOWN")
+    else:
+        print("ABORT SHUT DOWN")
+
+shut_down()
