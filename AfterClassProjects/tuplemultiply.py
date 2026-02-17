61eb40eee1b9d02ef1c68b6ac8a66ec1c1be48f6
Vaishnav Premjith <vaishnavpx@gmail.com>
2026-02-16 17:04:33 -0800

    tuple products

---
+++ AfterClassProjects/tuplemultiply.py
@@ -0,0 +1,12 @@
+tup1=(4,3,3,2,-1,18)
+tup2=(2,4,8,8,3,2,9)
+#tup1
+
+prod1=tup1[0]*tup1[1]*tup1[2]*tup1[3]*tup1[4]*tup1[5]
+print("product 1 is:")
+print(prod1)
+#tup2
+
+prod2=tup2[0]*tup2[1]*tup2[2]*tup2[3]*tup2[4]*tup2[5]*tup2[6]
+print("product 2 is:")
+print(prod2)
