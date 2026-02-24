603ebf30909b6245f37414c5145263a99bc4380c
Vaishnav Premjith <vaishnavpx@gmail.com>
2026-02-23 16:38:05 -0800

    Symmetric Difference

---
+++ AfterClassProjects/SymetricDifference.py
@@ -0,0 +1,17 @@
+setx={"green","blue"}
+sety={"blue","yellow"}
+print("original set elements: ")
+print(setx)
+print(sety)
+print("\nsymmetric difference of two said sets: ")
+setz=setx.symmetric_difference(sety)
+print(setz)
+
+setx={1,2,3,4,5}
+sety={1,5,6,7,8,9}
+print("original set elements: ")
+print(setx)
+print(sety)
+print("\nsymmetric difference of two said sets: ")
+setz=setx.symmetric_difference(sety)
+print(setz)
