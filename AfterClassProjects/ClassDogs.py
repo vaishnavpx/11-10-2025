c7d39dc3845be5ce02641062b73c6fe6f71dbe6b
Vaishnav Premjith <vaishnavpx@gmail.com>
2026-03-02 18:32:08 -0800

    Dog Breed Class

---
+++ AfterClassProjects/ClassDogs.py
@@ -0,0 +1,13 @@
+class Dog:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+
+
+Dog1=Dog("Max",4)
+print("First Dog's name: ",Dog1.name)
+print("First Dog's age: ",Dog1.age)
+
+Dog2=Dog("Beau",5)
+print("Second Dog's name: ",Dog2.name)
+print("Second Dog's age: ",Dog2.age)
