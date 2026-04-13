2bd85c9a00fdeb16fe148e10d7b16aba6300c644
Vaishnav Premjith <vaishnavpx@gmail.com>
2026-04-13 16:55:19 -0700

    my first game screen

--- Pygame1/main.py
+++ Pygame1/main.py
@@ -1,23 +1,22 @@
 import pygame
-pygame.init
+pygame.init()
 SCREEN_WIDTH,SCREEN_HEIGHT=500,500
 
 display_surface=pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption("Adding image and background image")
 
-background_image=pygame.transform.scale(
-    pygame.image.load("Background.jpg").convert(),
-    (SCREEN_WIDTH,SCREEN_HEIGHT))
+background_color = (58, 58, 58)  # Blue
 
 character_image=pygame.transform.scale(
-    pygame.image.load("Character.png").convert_alpha(),(200,200))
+    pygame.image.load("Character.png").convert_alpha(),(300,300))
 
 character_rect=character_image.get_rect(center=(SCREEN_WIDTH//2,
                                                 SCREEN_HEIGHT//2-30))
 
-text=pygame.font.Font(None,36).render("Hello World",True,
+text=pygame.font.Font(None,36).render("My First Game Screen.",True,
                                       pygame.Color("black"))
 
+text_rect=text.get_rect(center=(SCREEN_WIDTH//2,SCREEN_HEIGHT//2+110))
 def game_loop():
     clock=pygame.time.Clock()
     running=True
@@ -26,14 +25,16 @@ def game_loop():
             if event.type==pygame.QUIT:
                 running=False
 
-        display_surface.blit(background_image,(0,0))
         display_surface.blit(character_image,character_rect)
         display_surface.blit(text,text_rect)
 
         pygame.display.flip()
         clock.tick(30)
 
-    pygame.quit
+    display_surface.fill(background_color)
+    pygame.display.flip()
+
+    pygame.quit()
 
 if __name__=="__main__":
     game_loop()
