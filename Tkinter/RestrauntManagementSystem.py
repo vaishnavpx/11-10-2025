e72ffac65b84ef56f68182555e05bcb879cd56c9
Vaishnav Premjith <vaishnavpx@gmail.com>
2026-06-07 21:24:49 -0700

    Restauraunt Management System

--- Tkinter/RestrauntManagementSystem.py
+++ Tkinter/RestrauntManagementSystem.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import ttk,messagebox
 
 class RestauraunOrderManagement:
-    def __init__(self,,root):
+    def __init__(self,root):
         self.root=root
         self.root.title=("Restauraunt Management App")
         self.menu_items={
@@ -73,7 +73,45 @@ class RestauraunOrderManagement:
         canvas.pack()
         original_image=tk.PhotoImage(file="Restaurauntpic.jpg")
         background_image=original_image.subsample(
-            original_image.width()//bg_width
+            original_image.width()//bg_width,
             original_image.height()//bg_height)
         canvas.create_image(0,0,anchor=tk.NW,image=background_image)
-        
+        canvas.image=background_image
+
+
+    def update_menu_prices(self,*args):
+        currency=self.currency_var.get()
+        symbol="₹" if currency=="INR" else "$"
+        rate=self.exchange_rate if self.currency=="INR" else 1
+        for item,label in self.menu_labels.items():
+            price=self.menu_items[item]*rate
+            label.config(text=f"{item} ({symbol}{price}):")
+
+    def place_order(self):
+        total_cost=0
+        order_summary="Order Summary:\n"
+        currency=self.currency_var.get()
+        symbol="₹" if currency=="INR" else "$"
+        rate=self.exchange_rate if currency=="INR" else 1
+        for item,entry in self.menu_quantities.items():
+            quantity=entry.get()
+            if quantity.isdigit():
+                quantity=int(quantity)
+                price=self.menu_items[item]*rate
+                cost=quantity*price
+                total_cost+=cost
+                if quantity>0:
+                    order_summary+=f"{item}: {quantity} X {symbol}{price} = {symbol}{cost}\n"
+        if total_cost>0:
+            order_summary+=f"Total Cost: {symbol}{total_cost}"
+            messagebox.showinfo(
+                "Order Placed",
+                order_summary)
+        else:
+            messagebox.showerror("Error","Please order at least one item")
+
+if __name__=="__main__":
+    root=tk.Tk()
+    app=RestauraunOrderManagement(root)
+    root.geometry("800x600")
+    root.mainloop()
